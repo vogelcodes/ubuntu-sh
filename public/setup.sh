8697ba468d8e8a76c1d4dc5e256c30dc85d97509
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2024-02-02 18:28:44 +0000

    addsgithub cli and login

--- public/setup.sh
+++ public/setup.sh
@@ -24,6 +24,14 @@ sudo systemctl enable docker
 # Install Python
 sudo apt install python3 unzip -y
 
-curl -fsSL https://bun.sh/install | bash 
+curl -fsSL https://bun.sh/install | bash
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+gh auth login
 
 echo "Setup completed. You may need to restart your shell to apply changes."
